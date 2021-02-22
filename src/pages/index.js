2fd0f0b75407696bacbde117a321da8d31dca7f2
Knude <19505035+Knude@users.noreply.github.com>
2021-02-22 19:05:44 +0100

    Changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import theme from "theme";
-import { Theme, Link, Strong, Text, Button, Icon, Box, Image } from "@quarkly/widgets";
+import { Theme, Link, Strong, Text, Button, Box, Icon, Image } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
 import { GlobalQuarklyPageStyles } from "global-page-styles";
 import { Override, StackItem, Stack, Section } from "@quarkly/components";
@@ -186,6 +186,26 @@ export default (() => {
 				Bestil tid
 			</Button>
 		</Section>
+		<Section padding="140px 0" sm-padding="40px 0" background="url(https://images.unsplash.com/photo-1515600051222-a3c338ff16f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80) center/cover">
+			<Box max-width="240px" padding="50px 80px 80px 50px" background="--color-light" color="--dark">
+				<Text
+					as="h4"
+					font="--base"
+					color="--grey"
+					letter-spacing="1px"
+					text-transform="uppercase"
+					margin="6px 0"
+				>
+					Frisør
+				</Text>
+				<Text as="h2" font="--headline2" margin="0 0 12px 0">
+					Karina Rahr
+				</Text>
+				<Text font="--base">
+					My name is David R. Moore, born and raised in France, worked as a professional photographer and videographer for more than 10 years, awarded the CDS Documentary Essay Prize in 2018 and Best Photographer of The Year 2019 by Sony World Photography Awards.
+				</Text>
+			</Box>
+		</Section>
 		<Section color="--dark">
 			<Text
 				as="h2"
@@ -221,7 +241,6 @@ export default (() => {
 					margin="10px 0"
 					color="--greyD2"
 					text-align="center"
-					width="1150px"
 				>
 					<Strong>
 						Mandag{" "}
